f5fa25e81a0a1397415f2d3c5cdf16267d69419a
Muhammet Ilendemli <ilendemli@live.at>
2014-05-12 22:28:44 +0200

    updated for the new login procedure
    sony, don't be like google and update it every month, please

--- index.php
+++ index.php
@@ -1,212 +1,216 @@
 <?php
-	require_once 'scripts/login.php';
-	require_once 'scripts/parser.php';
-	require_once 'scripts/Toro.php';
-	require_once 'site/site.php';
-	
-	ToroHook::add('404', function() {
-		echo 'Not found';
-	});
-
-	Toro::serve(array(
-		'/' => 'Site',
-		'/login' => 'Login',
-		'/login/refresh' => 'Refresh',
-		'/me' => 'Me',
-		'/profile' => 'Profile',
-		'/profile/state' => 'State',
-		'/friends' => 'Friends',
-		'/trophies' => 'Trophies',
-		'/trophies/game' => 'GameTrophies',
-		'/trophies/game/trophy' => 'TrophyById',
-		'/conversations' => 'Conversations',
-		'/conversations/chat' => 'Chat',
-		'/conversations/chat/message' => 'Message'
-	));
-	
-	class State {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
+	class PSNLogIn {
+		private $psnURLS, $psnVars;
+		private $email, $password;
+		private $cookieDir, $cookieFile;
+				
+		public function __construct() {
+			$this->cookieDir = "./tmp/";
+			$this->cookieFile = $this->cookieDir ."psn_" .$_SERVER["REMOTE_ADDR"] .".cookie";
 			
-			$parser = new PSNParser($region, $language);
+			$this->psnVars = array(
+				'SENBaseURL' => 'https://auth.api.sonyentertainmentnetwork.com',
+				'redirectURL_oauth' => 'com.scee.psxandroid.scecompcall://redirect',
+				'client_id' => 'b0d0d7ad-bb99-4ab1-b25e-afa0c76577b0',
+				'scope' => 'sceapp',
+				'scope_psn' => 'psn:sceapp',
+				'csrfToken' => '',
+				'authCode' => '',
+				'client_secret' => 'Zo4y8eGIa3oazIEp',
+				'duid' => '00000005006401283335353338373035333434333134313a433635303220202020202020202020202020202020',
+				'cltm' => '1399637146935',
+				'service_entity' => 'urn:service-entity:psn'
+			);
 			
-			echo $parser->getState($accessToken, $profileId);
-		}
-	}
-	
-	class TrophyById {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			$gameId = $_POST['gameId'];
-			$trophyId = $_POST['trophyId'];
-			
-			$parser = new PSNParser($region, $language);
-	
-			echo $parser->getTrophyById($accessToken, $profileId, $gameId, $trophyId);
-		}
-	}
-	
-	class GameTrophies {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			$gameId = $_POST['gameId'];
-			
-			$parser = new PSNParser($region, $language);
-	
-			echo $parser->getGameTrophies($accessToken, $profileId, $gameId);
-		}
-	}
-	
-	class Message {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$chatId = $_POST['chatId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			$message = $_POST['message'];
-			
-			$parser = new PSNParser($region, $language);
-			
-			echo $parser->sendMessage($accessToken, $chatId, $message);
-		}
-	}
-	
-	class Chat {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$chatId = $_POST['chatId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			
-			$parser = new PSNParser($region, $language);
-			
-			echo $parser->getChat($accessToken, $chatId);
-		}
-	}
-	
-	class Conversations {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-	
-			$parser = new PSNParser($region, $language);
-	
-			echo $parser->getConversations($accessToken, $profileId);
-		}
-	}
-			
-	class Trophies {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			$iconSize = $_POST['iconSize'];
-			$offset = $_POST['offset'];
-			$limit = $_POST['limit'];
-			
-			$parser = new PSNParser($region, $language);
-	
-			echo $parser->getTrophies($accessToken, $profileId, $iconSize, $offset, $limit);
-		}
-	}
-	
-	class Friends {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			
-			$parser = new PSNParser($region, $language);
-			
-			echo $parser->getFriendlist($accessToken, $profileId);
-		}
-	}
-	
-	class Profile {
-		function post() {			
-			$accessToken = $_POST['accessToken'];
-			$profileId = $_POST['profileId'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			
-			$parser = new PSNParser($region, $language);
-			
-			echo $parser->getProfile($accessToken, $profileId);
-		}
-	}
-			
-	class Me {
-		function post() {
-			$accessToken = $_POST['accessToken'];
-			$region = $_POST['region'];
-			$language = $_POST['language'];
-			
-			$parser = new PSNParser($region, $language);
-			
-			echo $parser->getMyInfos($accessToken);
-		}
-	}
-	
-	class Refresh {
-		function post() {
-			$refreshToken = $_POST['refreshToken'];
-			
-			$login = new PSNLogIn();
-			
-			echo $login->refreshTokens($refreshToken);
-		}
-	}
-	
-	class Login {
-		private $emailRegex;
-		
-		function get() {
-			?>
-			<form action="./login" method="post">
-				<input type="email" name="email" placeholder="e-Mail" />
-				<input type="password" name="password" placeholder="Password" />
-				<input type="submit" value="LogIn" />
-			</form>
-			<?php
+			$this->psnURLS = array(
+				'signIn' => $this->psnVars['SENBaseURL'] .'/2.0/oauth/authorize?response_type=code&service_entity=' .$this->psnVars['service_entity'] .'&returnAuthCode=true&cltm=' .$this->psnVars['cltm'] .'&redirect_uri=' .$this->psnVars['redirectURL_oauth'] .'&client_id=' .$this->psnVars['client_id'] .'&scope=' .$this->psnVars['scope_psn'],
+				'signInPost' => $this->psnVars['SENBaseURL'] .'/login.do',
+				'oauth' => 'https://auth.api.sonyentertainmentnetwork.com/2.0/oauth/token'
+			);
 		}
 		
-		function __construct() {
-			$this->emailRegex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/'; 
+		public function setEmail($email) {
+			$this->email = $email;
 		}
-	
-		function post() {
-			$email = $_POST['email'];
-			$password = $_POST['password'];
+		
+		public function setPassword($password) {
+			$this->password = $password;
+		}
+		
+		public function initCURL() {
+			if(file_exists($this->cookieFile))	
+				unlink($this->cookieFile);
+		
+			$this->curl = curl_init();
 			
-			if (preg_match($this->emailRegex, $email)) {
-				 if (!empty($password)) {
-					$login = new PSNLogIn();
+			$options = array(
+				CURLOPT_RETURNTRANSFER => TRUE,
+				CURLOPT_SSL_VERIFYPEER => FALSE,
+				CURLOPT_FOLLOWLOCATION => FALSE,
+				CURLOPT_COOKIEFILE => $this->cookieFile
+			);
 
-					$login->setEmail($email);
-					$login->setPassword($password);
-					
-					echo $login->login();
+			curl_setopt_array($this->curl, $options);
 			
-				 } else {
-					echo 'Please enter your password!';
-				 }
-				 
-			} else { 
-				 echo 'Please enter a valid e-Mail!';
-			} 
+			touch($this->cookieFile);
+		}
+		
+		public function closeCURL() {
+			curl_close($this->curl);
+			
+			if(file_exists($this->cookieFile))	
+				unlink($this->cookieFile);
+		}
+		
+		public function login() {
+			$this->initCURL();
+			
+			$options = array(
+				CURLOPT_URL => $this->psnURLS['signIn'],
+				CURLOPT_HEADER => FALSE,
+				CURLOPT_POST => FALSE
+			);
+
+			curl_setopt_array($this->curl, $options);
+			
+			$output = curl_exec($this->curl);
+			$header = $this->get_headers_from_curl_response($output);
+			
+			$tokens = $this->getAuth($header['Location']);
+			
+			$this->closeCURL();
+			
+			echo json_encode($tokens);
+		}
+		
+		private function getAuth($location) {
+			$headerData = array(
+				'Origin: https://auth.api.sonyentertainmentnetwork.com',
+				'Referer: ' .$location
+			);
+		
+			$postData = array(
+				'j_username' => $this->email,
+				'j_password' => $this->password,
+				'params' => 'service_entity=psn'
+			);
+			
+			$options = array(
+				CURLOPT_URL => $this->psnURLS['signInPost'],
+				CURLOPT_POST => TRUE,
+				CURLOPT_HEADER => TRUE,
+				CURLOPT_POSTFIELDS => http_build_query($postData),
+				CURLOPT_HTTPHEADER => $headerData
+			);
+
+			curl_setopt_array($this->curl, $options);
+			
+			$output = curl_exec($this->curl);
+			$header = $this->get_headers_from_curl_response($output);
+			
+			$options = array(
+				CURLOPT_URL => $header['Location'],
+				CURLOPT_POST => FALSE,
+				CURLOPT_HEADER => TRUE
+			);
+
+			curl_setopt_array($this->curl, $options);
+			
+			$output = curl_exec($this->curl);
+			$header = $this->get_headers_from_curl_response($output);
+			
+			$location = urldecode($header['Location']);
+			$authCode = substr($location, strpos($location, 'authCode=') +9, 6);
+			
+			return $this->getAccessToken($authCode);
+		}
+		
+		function get_headers_from_curl_response($response) {
+			$headers = array();
+			$header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+			foreach(explode("\r\n", $header_text) as $i => $line) {
+				if($i === 0) {
+					$headers['http_code'] = $line;
+				} else {
+					list ($key, $value) = explode(': ', $line);
+
+					$headers[$key] = $value;
+				}
+			}
+
+			return $headers;
+		}
+		
+		private function getAccessToken($authCode) {
+			$dataArray = array(
+				'grant_type' => 'authorization_code',
+				'client_id' => $this->psnVars['client_id'],
+				'client_secret' => $this->psnVars['client_secret'],
+				'code' => $authCode,
+				'redirect_uri' => $this->psnVars['redirectURL_oauth'],
+				'state' => 'x',
+				'scope' => $this->psnVars['scope_psn'],
+				'duid' => $this->psnVars['duid']
+			);
+			
+			$postData = http_build_query($dataArray);
+			
+			$options = array(
+				CURLOPT_URL => $this->psnURLS['oauth'],
+				CURLOPT_POST => TRUE,
+				CURLOPT_HEADER => FALSE,
+				CURLOPT_POSTFIELDS => $postData
+			);
+
+			curl_setopt_array($this->curl, $options);
+			
+			$output = curl_exec($this->curl);			
+			$jsonData = json_decode($output, true);
+			
+			return $jsonData;
+		}
+		
+		public function refreshTokens($refreshToken) {
+			$this->initCURL();
+			
+			$dataArray = array(
+				'grant_type' => 'refresh_token',
+				'client_id' => $this->psnVars['client_id'],
+				'client_secret' => $this->psnVars['client_secret'],
+				'refresh_token' => $refreshToken,
+				'redirect_uri' => $this->psnVars['redirectURL'],
+				'state' => 'x',
+				'scope' => $this->psnVars['scope_psn'],
+				'duid' => $this->psnVars['duid']
+			);
+			
+			$postData = http_build_query($dataArray);
+			
+			curl_setopt($this->curl, CURLOPT_POSTFIELDS, $postData);
+			curl_setopt($this->curl, CURLOPT_URL, $this->psnURLS['oauth']);
+			
+			$output = curl_exec($this->curl);
+			$this->closeCURL();
+			
+			$jsonData = json_decode($output, true);
+			
+			if (empty($jsonData['access_token']) || empty($jsonData['refresh_token'])) {
+				return -4;
+				
+			} else {
+				$toReturn = array(
+					'accessToken' => $jsonData['access_token'],
+					'refreshToken' => $jsonData['refresh_token']
+				);
+				
+				return json_encode($toReturn);
+			}
+		}
+		
+		public function print_r($r) {
+			echo '<pre>';
+			print_r($r);
+			echo '</pre>';
 		}
 	}
 ?>
