97e82f9dd5c7d54b0208575a7a8851614bd7d984
Muhammet Ilendemli <ilendemli@live.at>
2014-05-12 22:26:49 +0200

    some small changes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 	require_once 'scripts/login.php';
 	require_once 'scripts/parser.php';
 	require_once 'scripts/Toro.php';
@@ -11,15 +11,60 @@
 	Toro::serve(array(
 		'/' => 'Site',
 		'/login' => 'Login',
-		'/refresh' => 'Refresh',
+		'/login/refresh' => 'Refresh',
 		'/me' => 'Me',
 		'/profile' => 'Profile',
+		'/profile/state' => 'State',
 		'/friends' => 'Friends',
-		'/trophies' => 'Trophy',
-		'/conversations' => 'Conversation',
-		'/chat' => 'Chat',
-		'/chat/message' => 'Message'
+		'/trophies' => 'Trophies',
+		'/trophies/game' => 'GameTrophies',
+		'/trophies/game/trophy' => 'TrophyById',
+		'/conversations' => 'Conversations',
+		'/conversations/chat' => 'Chat',
+		'/conversations/chat/message' => 'Message'
 	));
+	
+	class State {
+		function post() {
+			$accessToken = $_POST['accessToken'];
+			$profileId = $_POST['profileId'];
+			$region = $_POST['region'];
+			$language = $_POST['language'];
+			
+			$parser = new PSNParser($region, $language);
+			
+			echo $parser->getState($accessToken, $profileId);
+		}
+	}
+	
+	class TrophyById {
+		function post() {
+			$accessToken = $_POST['accessToken'];
+			$profileId = $_POST['profileId'];
+			$region = $_POST['region'];
+			$language = $_POST['language'];
+			$gameId = $_POST['gameId'];
+			$trophyId = $_POST['trophyId'];
+			
+			$parser = new PSNParser($region, $language);
+	
+			echo $parser->getTrophyById($accessToken, $profileId, $gameId, $trophyId);
+		}
+	}
+	
+	class GameTrophies {
+		function post() {
+			$accessToken = $_POST['accessToken'];
+			$profileId = $_POST['profileId'];
+			$region = $_POST['region'];
+			$language = $_POST['language'];
+			$gameId = $_POST['gameId'];
+			
+			$parser = new PSNParser($region, $language);
+	
+			echo $parser->getGameTrophies($accessToken, $profileId, $gameId);
+		}
+	}
 	
 	class Message {
 		function post() {
@@ -48,7 +93,7 @@
 		}
 	}
 	
-	class Conversation {
+	class Conversations {
 		function post() {
 			$accessToken = $_POST['accessToken'];
 			$profileId = $_POST['profileId'];
@@ -61,7 +106,7 @@
 		}
 	}
 			
-	class Trophy {
+	class Trophies {
 		function post() {
 			$accessToken = $_POST['accessToken'];
 			$profileId = $_POST['profileId'];
@@ -104,7 +149,7 @@
 	}
 			
 	class Me {
-		function get() {
+		function post() {
 			$accessToken = $_POST['accessToken'];
 			$region = $_POST['region'];
 			$language = $_POST['language'];
@@ -127,6 +172,16 @@
 	
 	class Login {
 		private $emailRegex;
+		
+		function get() {
+			?>
+			<form action="./login" method="post">
+				<input type="email" name="email" placeholder="e-Mail" />
+				<input type="password" name="password" placeholder="Password" />
+				<input type="submit" value="LogIn" />
+			</form>
+			<?php
+		}
 		
 		function __construct() {
 			$this->emailRegex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/'; 
