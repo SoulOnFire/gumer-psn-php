517fb3a63823ab673c0c39f887e5629efbd8ba62
Bradley Weston <brad@legalweb.org.uk>
2014-06-06 22:39:46 +0100

    Added PHPUnit
    Added PHPUnit along with first test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-if (! file_exists('../vendor/autoload.php'))
+if (! file_exists(__DIR__ . '/../vendor/autoload.php'))
 {
 	echo 'You must run `composer install` before running tests.';
 	exit;
 }
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
  // @todo: Set Guzzle plugin for mock.
